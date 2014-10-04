e460f6481617bc5b5742f5c3f5fbbb6ff5840096
tynsh <tynsh@shackspace.de>
2014-10-04 17:27:28 +0200

    fixed spelling mistakes

--- printtoken/printbon.sh
+++ printtoken/printbon.sh
@@ -2,6 +2,6 @@
 
 lpr -o DocCutType=0NoCutDoc -P 'Star_TSP143_' /opt/printbon/1.eps
 
-/opt/printbon/simple.sh 2880 1 2>/dev/null | grep -Po 'code\" : "\d[^"]*' | grep -Po '\d*' | sed 's/./&-/5' | lpr -o cpi=10 -o DocCutType=0NoCutDoc -P 'Star_TSP143_'
+/opt/printbon/simple.sh 2880 1 2>/dev/null | grep -Po 'code\" : "\d[^"]*' | grep -Po '\d*' | sed 's/./&-/5' | sed 's/^/ /g' | lpr -o cpi=10 -o DocCutType=0NoCutDoc -P 'Star_TSP143_'
 
 lpr -P 'Star_TSP143_' /opt/printbon/2.eps
